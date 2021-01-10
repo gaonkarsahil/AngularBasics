b4342ffd60ff854aecb6e92b49cf498c64ed5a53
gaonkarsahil <sahilgaonkar24@gmail.com>
2021-01-10 18:46:31 +0530

    0009 created component for classbinding.

--- SahilHelloWorld-Project/src/app/app.module.ts
+++ SahilHelloWorld-Project/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { InterpolationlearnComponent } from './interpolationlearn/interpolationlearn.component';
 import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { ClassbindingComponent } from './classbinding/classbinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     InterpolationlearnComponent,
-    PropertybindingComponent
+    PropertybindingComponent,
+    ClassbindingComponent
   ],
   imports: [
     BrowserModule,
